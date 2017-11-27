65cce1ae06e01c9eade151042ca7402aa780c9d6
Okanlawon Emmanuel <okanlawonemmanuel41@gmail.com>
2017-11-27 09:41:59 +0000

    lazyloading off screen images

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,12 @@
 
-// // Variable Declearation
+// // Declearation
+
+const images = document.querySelectorAll('img[data-src]');
+const config = {
+    rootMargin: '50px 0px',
+    threshold: 0.01
+};
+let observer;
 
 // let search = document.getElementById("search");
 // let hero = document.getElementsByClassName("hero");
@@ -20,3 +27,31 @@
 //      hero[0].classList.toggle("hero__form-focus");
 
 // }
+
+
+
+
+
+if ('IntersectionObserver' in window) {
+  observer = new IntersectionObserver(onChange, config);
+  images.forEach(img => observer.observe(img));
+} else {
+  console.log('%cIntersection Observers not supported', 'color: red');
+  images.forEach(image => loadImage(image));
+}
+
+const loadImage = image => {
+  image.classList.add('fade-in');
+  image.src = image.dataset.src;
+}
+
+function onChange(changes, observer) {
+  changes.forEach(change => {
+    if (change.intersectionRatio > 0) {
+      // Stop watching and load the image
+      loadImage(change.target);
+      observer.unobserve(change.target);
+    }
+    
+  });
+}
