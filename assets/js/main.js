28bd80f041124c045f8e0920d5cdef38510065d8
Okanlawon Emmanuel <okanlawonemmanuel41@gmail.com>
2017-11-28 08:50:41 +0000

    responsiveness

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,9 @@ const config = {
     threshold: 0.01
 };
 let observer;
+let toggle = document.getElementById("toggle");
+let nav = document.getElementById("nav");
+let navLinks = nav.children[0].children;
 
 // let search = document.getElementById("search");
 // let hero = document.getElementsByClassName("hero");
@@ -15,17 +18,37 @@ let observer;
 
 // search.addEventListener("focus", expandHero);
 // search.addEventListener("focusout", expandHero);
+toggle.addEventListener("click", toggleNav);
+Array.prototype.forEach.call(navLinks, function (nav, index) {
+console.log(nav.children[0])
+nav.children[0].addEventListener("click", gotoSection)
 
 
+})
 // // Functions
 
+function gotoSection (e) {
+  e.preventDefault();
+if (nav.classList.contains("active")) nav.classList.toggle("active");
+elementIdName = e.target.attributes.href.value
+element = document.getElementById(elementIdName);
+
+smoothScroll(element);
+};
 
 // function expandHero (e) {
 //      hero[0].classList.toggle("hero__form-focus");
 
 // }
 
+function toggleNav() {
+
+nav.classList.toggle("active")
+
+}
+
 window.onload = function () {
+
 
 if ('IntersectionObserver' in window) {
   observer = new IntersectionObserver(onChange, config);
@@ -52,4 +75,28 @@ function onChange(changes, observer) {
 }
 
 
+}
+
+
+function smoothScroll (target) {
+    var scrollContainer = target;
+    do { //find scroll container
+        scrollContainer = scrollContainer.parentNode;
+        if (!scrollContainer) return;
+        scrollContainer.scrollTop += 1;
+    } while (scrollContainer.scrollTop == 0);
+    
+    var targetY = 0;
+    do { //find the top of target relatively to the container
+        if (target == scrollContainer) break;
+        targetY += target.offsetTop;
+    } while (target = target.offsetParent);
+    
+    scroll = function(c, a, b, i) {
+        i++; if (i > 30) return;
+        c.scrollTop = a + (b - a) / 30 * i;
+        setTimeout(function(){ scroll(c, a, b, i); }, 20);
+    }
+    // start scrolling
+    scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
 }
